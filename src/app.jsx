a36d644758ddf80073e52a047ba53ebd9f0b7ec1
BreeArmstrong <breearmstrong@ARCMACBOOK31.local>
2023-04-13 11:04:48 -0400

    beginning files

---
+++ src/app.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import {
+  Stickynav,
+  ProductJumbo,
+  ProductText,
+  ProductCallout,
+} from "arccorp-vars";
+
+class App extends Component {
+  constructor() {
+    super();
+  }
+
+  render() {
+    return (
+    <div>Hello World</div>
+    );
+  }
+}
+
+export default App;
